10d8f177b850a6f524bbfc9990eddcb1d3c843d3
seimonik <simonik1002@mail.ru>
2023-05-01 14:35:23 +0400

    доработка

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 // InputMismatchException выдается сканером, чтобы указать, что извлеченный токен не соответствует шаблону
 // для ожидаемого типа или что токен находится вне диапазона для ожидаемого типа.
@@ -13,12 +11,28 @@ public class Main {
             int count = in.nextInt();
 
             ArrayList<Student> students = new ArrayList<Student>();
-            System.out.println("Введите имена студентов и информацию о стипендии (1 - отличник, 2 - повышенная, 3 - президентская, 4 - без надбавок, 0 - обнуление)");
+            System.out.println("Введите имя студента, уровень образования (бакалавр, магистр, аспирант)\nи информацию о стипендии (1 - отличник, 2 - повышенная, 3 - президентская, 4 - без надбавок, 0 - обнуление)");
+            String[] levels = new String[]{"бакалавр", "магистр", "аспирант"};
             for (int i = 0; i < count; i++) {
-                students.add(new Student(in.next()));
+                String name = in.next();
+                String level = in.next().toLowerCase();
+                if(!Arrays.asList(levels).contains(level)){
+                    throw new IllegalArgumentException("Введен несуществующий уровень");
+                }
+                switch (level){
+                    case "бакалавр":
+                        students.add(new UndergraduateStudent(name));
+                        break;
+                    case "магистр":
+                        students.add(new MastersDegreeStudent(name));
+                        break;
+                    case "аспирант":
+                        students.add(new PostgraduateStudent(name));
+                        break;
+                }
                 int command = in.nextInt();
                 if(command < 0 || command > 4){
-                    throw new RuntimeException("Выход за диапазон доступных команд");
+                    throw new IllegalArgumentException("Выход за диапазон доступных команд");
                 }
                 switch (command){
                     case 0:
@@ -41,7 +55,7 @@ public class Main {
                 System.out.println(stud);
             }
         } catch (InputMismatchException e){
-            System.out.println("Введите число");
+            System.out.println("Необходимо ввести число");
         } catch (RuntimeException e){
             System.out.println(e.getMessage());
         }
--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,5 @@
-public class Student {
-    private String Name;
-    private double Scholarship=4211.71;
-    public Student(String name){
-        this.Name=name;
-    }
-    public double getScholarship(){
-        return this.Scholarship;
-    }
-    public void setScholarship(double value){
-        this.Scholarship=value;
-    }
-    @Override
-    public String toString(){
-        return Name + " gets " + Scholarship;
-    }
+public interface Student {
+    public double getScholarship();
+    public void setScholarship(double value);
+    public String toString();
 }
